8c013526690ce3fd2b2c1c4240a31aaa792f5fe0
Jiaqi Lu <jiaqi.lu@hpc-submit01.scidom.de>
2026-01-29 13:41:35 +0100

    update path

--- streamlit/utils_sg/utils_single_core.py
+++ streamlit/utils_sg/utils_single_core.py
@@ -111,28 +111,28 @@ def load_resources(cfg: SimulationConfig) -> Tuple[Any, np.lib.npyio.NpzFile, Di
     
     # Read differnet datasets
     if cfg.orig_data_name == "Gasperini (high MOI)": 
-        model_dir = Path("../save_model_gasperini/model")
-        # model_dir = Path("/srv/perturbo/st-single-gene-calculator/save_model_gasperini/model")
+        # model_dir = Path("../save_model_gasperini/model")
+        model_dir = Path("/srv/perturbo/st-single-gene-calculator/save_model_gasperini/model")
         mdata_tiny = md.read_h5mu(f'{model_dir}/mdata_tiny.h5mu')
         print(f"Model is loaded from path: {model_dir}")
         model = perturbo.PERTURBO.load(model_dir, adata=mdata_tiny)
         _pristine_state = copy.deepcopy(model.module.state_dict())
 
-        real_path = Path("../save_model_gasperini/model/reference_stats_compact.npz")
-        # real_path = Path("/srv/perturbo/st-single-gene-calculator/save_model_gasperini/model/reference_stats_compact.npz")
+        # real_path = Path("../save_model_gasperini/model/reference_stats_compact.npz")
+        real_path = Path("/srv/perturbo/st-single-gene-calculator/save_model_gasperini/model/reference_stats_compact.npz")
         mdata_real = np.load(real_path, allow_pickle=True)
         print(f"mdata_real: {mdata_real}")
         
     elif cfg.orig_data_name == "Weissman (low MOI)": 
-        model_dir = Path("../save_model_replogle/model")
-        # model_dir = Path("/srv/perturbo/st-single-gene-calculator/save_model_replogle/model")
+        # model_dir = Path("../save_model_replogle/model")
+        model_dir = Path("/srv/perturbo/st-single-gene-calculator/save_model_replogle/model")
         mdata_tiny = md.read_h5mu(f'{model_dir}/mdata_tiny.h5mu')
         print(f"Model is loaded from path: {model_dir}")
         model = perturbo.PERTURBO.load(model_dir, adata=mdata_tiny)
         _pristine_state = copy.deepcopy(model.module.state_dict())
 
-        real_path = Path("../save_model_replogle/model/reference_stats_compact.npz")
-        # real_path = Path("/srv/perturbo/st-single-gene-calculator/save_model_replogle/model/reference_stats_compact.npz")
+        # real_path = Path("../save_model_replogle/model/reference_stats_compact.npz")
+        real_path = Path("/srv/perturbo/st-single-gene-calculator/save_model_replogle/model/reference_stats_compact.npz")
         mdata_real = np.load(real_path, allow_pickle=True)
         print(f"mdata_real: {mdata_real}")
 
